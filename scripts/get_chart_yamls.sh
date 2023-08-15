161945fdd4a982056fefdd06a06f9d4708770742
Raz-Dahan <razdahan31@gmail.com>
2023-08-15 18:06:48 +0300

    Clean

--- scripts/get_chart_yamls.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "apiVersion: v2
-name: nasa-app
-version: 1.$BUILD_NUMBER.0
-description: A Helm chart for deploying Flask and Redis" > Chart.yaml
-
-echo "TAG: v1.$BUILD_NUMBER" > values.yaml
