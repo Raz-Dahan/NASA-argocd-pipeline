ffd7e31d17b4334d1223871c77d771927ffdf738
Raz-Dahan <razdahan31@gmail.com>
2023-08-23 17:42:27 +0300

    Add New Versioning

---
+++ versioning.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+
+LAST_COMMIT=$(git log -1 --pretty=%B)
+
+DOCKER_REPO='razdahan31/gha-pipeline'
+LATEST_TAG=$(curl -s "https://hub.docker.com/v2/repositories/${DOCKER_REPO}/tags" | jq -r '.results[0].name')
+
+ARRAY=($(echo "$LATEST_TAG" | awk -F. '{print $1" "$2" "$3}'))
+MAJOR=${ARRAY[0]}
+MINOR=${ARRAY[1]}
+PATCH=${ARRAY[2]}
+
+if [[ "${LAST_COMMIT,,}" == "major" ]]; then
+    MAJOR=$((MAJOR + 1))
+elif [[ "${LAST_COMMIT,,}" == "minor" ]]; then
+    MINOR=$((MINOR + 1))
+else
+    PATCH=$((PATCH + 1))
+fi
+
+
+NEW_TAG=$MAJOR.$MINOR.$PATCH
+
+echo $NEW_TAG
